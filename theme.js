863fbd6973da21eae1536aafde98d3e2abe0ac47
fanny <fanny@matierenoire.io>
2019-10-29 17:55:11 +0100

    style sidebar

--- theme.js
+++ theme.js
@@ -7,10 +7,61 @@ const theme = createMuiTheme({
     secondary: { main: palette.orangePrimary }
   },
   overrides: {
+    MuiSelect : {
+      select: {
+        backgroundColor: palette.greyExtraLight,
+        minWidth: '360px',
+        minHeight:'40px',
+      }
+    },
+    MuiInput : {
+      underline : {
+        '&:before' : {
+          borderBottom: 'none',
+          '&:hover' : {
+            borderBottom: 'none',
+          },
+        },
+      }
+    },
+    MuiSwitch: {
+      colorSecondary: {
+        '&$checked': {
+          color: palette.greenPrimary,
+          '&:hover' : {
+            backgroundColor: palette.greenOpacity,
+          },
+        },
+        '&$checked + $track': {
+          backgroundColor: palette.greenPrimary,
+        },
+        
+      },
+    },
+    MuiInputLabel: {
+      animated : {
+        color: palette.greyLight,
+        fontWeight: '700',
+        letterSpacing: 1,
+      }
+    },
     MuiButton: {
       root: {
         textTransform: 'none',
-        borderRadius: '2px'
+        borderRadius: '2px',
+      },
+      contained : {
+        boxShadow: 'none',
+        backgroundColor: palette.greyExtraLight,
+        color: palette.greyMedium,
+        '&:hover' : {
+          boxShadow: 'none',
+          backgroundColor: palette.greenOpacity,
+        },
+        '&:active' : {
+          boxShadow: 'none',
+          backgroundColor: palette.greenOpacity,
+        },
       },
       containedPrimary: {
         backgroundColor: palette.orangePrimary
