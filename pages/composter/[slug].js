4d36438487d1edfa6ecd4b80af87f393b85ef759
fanny <fanny@matierenoire.io>
2019-10-31 10:59:49 +0100

    correction header

--- pages/composter/[slug].js
+++ pages/composter/[slug].js
@@ -1,12 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 import api from '../../utils/api'
-import { Paper, Typography, AppBar, Button, Toolbar, Popper, List, ListItem, ListItemText, SvgIcon, Collapse } from '@material-ui/core'
+import { Paper, Typography, AppBar, Button, Toolbar, Popper, List, ListItem, ListItemText, ListItemIcon, Collapse, IconButton } from '@material-ui/core'
 import { ExpandMore, ExpandLess, Lens, ChevronLeft } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/styles'
 import palette from '../../variables'
-
-const options = ['Create a merge commit', 'Squash and merge', 'Rebase and merge']
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -18,12 +16,13 @@ const useStyles = makeStyles(theme => ({
   logo: {
     width: 80,
     height: 175,
-    backgroundColor: palette.greenPrimary
+    backgroundColor: palette.greenPrimary,
+    borderRadius: '0'
   },
   logoSvg: {
     display: 'Block',
     margin: '0 auto',
-    paddingTop: '50%'
+    paddingBottom: theme.spacing(8)
   },
   toolbarGlobal: {
     alignItems: 'flex-end'
@@ -33,59 +32,61 @@ const useStyles = makeStyles(theme => ({
   },
   toolbarLeft: {
     flexGrow: '1',
-    marginLeft: '30px'
+    marginLeft: theme.spacing(2)
+  },
+  chevronLeft: {
+    padding: '0'
   },
   UserButtonLog: {
     alignSelf: 'center',
-    marginBottom: '40px',
-    marginRight: '40px'
+    margin: theme.spacing(0, 5, 8, 0)
   },
   title: {
     color: palette.greyDark,
-    fontFamily: 'PT Sans',
     fontSize: '28px',
     fontWeight: '700'
   },
   button: {
     color: palette.greyDark,
-    fontFamily: 'PT Sans',
     fontSize: '16px',
     fontWeight: '400',
     borderBottomStyle: 'solid',
     borderBottomWidth: 3,
     borderBottomColor: 'transparent',
-    paddingTop: '40px',
-    marginRight: '20px',
-    paddingBottom: '25px',
+    padding: theme.spacing(6, 0, 3, 0),
+    marginRight: theme.spacing(4),
     '&:hover': {
       borderBottomColor: palette.greenPrimary,
       backgroundColor: 'white'
     }
   },
   nested: {
-    paddingLeft: theme.spacing(2),
-    backgroundColor: palette.orangeOpacity,
-    color: 'white',
-    borderBottomStyle: 'solid',
-    borderBottomWidth: '0.2px',
-    borderBottomColor: 'white',
-    '&:hover': {
-      backgroundColor: palette.orangePrimary
-    }
-  },
-  lens: {
-    width: '5px',
-    marginRight: '10px'
-  },
-  UserMenu: {
+    paddingLeft: theme.spacing(1),
     backgroundColor: palette.orangePrimary,
     color: 'white',
     borderBottomStyle: 'solid',
-    borderBottomWidth: '0.2px',
+    borderBottomWidth: '1px',
     borderBottomColor: 'white',
     '&:hover': {
       backgroundColor: palette.orangeOpacity
     }
+  },
+  nestedSecondary: {
+    backgroundColor: palette.orangeOpacity,
+    '&:hover': {
+      backgroundColor: palette.orangePrimary
+    }
+  },
+  arrow: {
+    color: 'white'
+  },
+  lens: {
+    width: '5px',
+    color: 'white'
+  },
+  listIcon: {
+    minWidth: '25px',
+    paddingLeft: theme.spacing(1)
   }
 }))
 
@@ -102,28 +103,21 @@ const Header = ({ title }) => {
   const handleClick = () => {
     setOpenSubMenu(!openSubMenu)
   }
-  const handleClose = event => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return
-    }
-
-    setOpen(false)
-  }
 
   return (
     <AppBar className={classes.appBar} position="static">
       <Toolbar className={classes.toolbarGlobal} disableGutters>
         <Link href="/" passHref>
-          <a className={classes.logo}>
+          <IconButton className={classes.logo}>
             <img className={classes.logoSvg} src="/static/logo2.svg" />
-          </a>
+          </IconButton>
         </Link>
         <div className={classes.toolbarLeft}>
           <div className={classes.toolbarTitle}>
             <Link href="/" passHref>
-              <a>
+              <IconButton className={classes.chevronLeft}>
                 <ChevronLeft />
-              </a>
+              </IconButton>
             </Link>
             <Typography variant="h1" className={classes.title}>
               {title}
@@ -138,34 +132,44 @@ const Header = ({ title }) => {
           </div>
         </div>
 
-        <Button onClick={handleToggle} color="secondary" ref={anchorRef} variant="contained" className={(classes.userButton, classes.UserButtonLog)}>
+        <Button
+          onClick={handleToggle}
+          color="secondary"
+          ref={anchorRef}
+          variant="contained"
+          className={(classes.userButton, classes.UserButtonLog)}
+          endIcon={open ? <ExpandLess /> : <ExpandMore />}
+        >
           Mon compte
-          {open ? <ExpandLess /> : <ExpandMore />}
         </Button>
 
         <Popper open={open} anchorEl={anchorRef.current} placement="bottom-end" transition disablePortal>
           <List component="nav">
-            <ListItem button className={classes.UserMenu}>
+            <ListItem button className={classes.nested}>
               <ListItemText primary="Mon Profil" />
             </ListItem>
 
-            <ListItem button className={classes.UserMenu} onClick={handleClick}>
+            <ListItem button className={classes.nested} onClick={handleClick}>
               <ListItemText primary="Mes composteurs" />
-              {openSubMenu ? <ExpandLess /> : <ExpandMore />}
+              <ListItemIcon>{openSubMenu ? <ExpandLess className={classes.arrow} /> : <ExpandMore className={classes.arrow} />}</ListItemIcon>
             </ListItem>
             <Collapse in={openSubMenu} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon><Lens /></ListItemIcon>
+                <ListItem button className={[classes.nested, classes.nestedSecondary].join(' ')}>
+                  <ListItemIcon className={classes.listIcon}>
+                    <Lens className={classes.lens} />
+                  </ListItemIcon>
                   <ListItemText primary="Composteur 1" />
                 </ListItem>
-                <ListItem button className={classes.nested}>
-                  <Lens className={classes.lens} />
+                <ListItem button className={[classes.nested, classes.nestedSecondary].join(' ')}>
+                  <ListItemIcon className={classes.listIcon}>
+                    <Lens className={classes.lens} />
+                  </ListItemIcon>
                   <ListItemText primary="Composteur 2" />
                 </ListItem>
               </List>
             </Collapse>
-            <ListItem button className={classes.UserMenu}>
+            <ListItem button className={classes.nested}>
               <ListItemText primary="Se déconnecter" />
             </ListItem>
           </List>
