76af4f43f6c38e7af31bcb7e1926a56fc6ef7437
JR <contact@jeanremypraud.com>
2019-11-05 15:28:01 +0100

    fecth le token au lancement de l'app coté serveur

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import App from 'next/app'
 import CssBaseline from '@material-ui/core/CssBaseline'
-
+import nextCookie from 'next-cookies'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from '../theme'
 import UserProvider from '../context/UserContext'
@@ -16,16 +16,27 @@ class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, token } = this.props
     return (
       <ThemeProvider theme={theme}>
-        <UserProvider>
+        <UserProvider token={token}>
           <CssBaseline />
           <Component {...pageProps} />
         </UserProvider>
       </ThemeProvider>
     )
   }
+}
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const { token } = nextCookie(ctx)
+  let pageProps = {}
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return { pageProps, token }
 }
 
 export default MyApp
