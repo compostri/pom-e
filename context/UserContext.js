31727f50c1a7b39a10e6bec29edb7c85579afdde
JR <contact@jeanremypraud.com>
2019-11-05 15:28:16 +0100

    gestion du token dans le context

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,4 +1,7 @@
 import React, { createContext, Component } from 'react'
+import api from '../utils/api'
+import cookie from 'js-cookie'
+import jwt_decode from 'jwt-decode'
 
 export const UserContext = createContext({})
 
@@ -7,11 +10,41 @@ class UserProvider extends Component {
     super(props)
     this.state = {
       isLoggedIn: false,
+      token: props.token || null,
       user: {},
+      login: async values => {
+        const res = await api.login(values)
+        // On set le cookie
+        if (res.data && res.data.token) {
+          cookie.set('token', res.data.token, { expires: 1 })
+          this.populateUser()
+        }
+      },
+      logout: () => {
+        cookie.remove('token')
+      },
       isLoggedIn: () => {
-        return !!this.state.user.id
+        return !!this.state.token
       }
     }
+  }
+
+  getUserInfosFromToken(tk) {
+    const token = tk || cookie.get('token')
+    if (token) {
+      const { iat, exp, ...rest } = jwt_decode(token)
+      return rest
+    }
+    return null
+  }
+
+  populateUser(token) {
+    const user = this.getUserInfosFromToken(token)
+    this.setState({ user, token })
+  }
+
+  componentDidMount() {
+    this.populateUser(this.state.token)
   }
 
   render = () => <UserContext.Provider value={{ userContext: this.state }}>{this.props.children}</UserContext.Provider>
