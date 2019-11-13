a7a8423d5d7a993c4b0b199d0d047713b1044e07
LucileB <lucileb@matierenoire.io>
2019-11-13 10:57:09 +0100

    modif page profil

--- pages/profil.js
+++ pages/profil.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import { Paper, Typography, Button, FormControlLabel, TextField, Switch } from '@material-ui/core'
+import { Paper, Button, FormControlLabel, TextField, Switch, Tabs, Tab, Container, Box, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import classNames from 'classnames'
+import palette from '../variables'
 
 const useStyle = makeStyles(theme => ({
   sectionProfil: {
-    width: 800,
-    padding: theme.spacing(5),
-    marginLeft: 370,
-    marginTop: 180,
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between'
+    padding: theme.spacing(2, 5, 5, 5),
+    margin: theme.spacing(10, 15)
   },
   buttonSubmit: {
     display: 'block',
-    margin: '20px auto 0',
-    color: 'white'
+    margin: '25px auto 0',
+    padding: theme.spacing(2)
   },
   newsletter: {
-    padding: 10,
-    top: 70
+    padding: 4,
+    top: 70,
+    color: palette.greyLight,
+    display: 'block'
   },
   title: {
     top: 15,
@@ -44,48 +42,135 @@ const useStyle = makeStyles(theme => ({
     width: '48%',
     marginBottom: theme.spacing(2),
     marginRight: '2%'
+  },
+  appBar: {
+    alignItems: 'baseline',
+    justifyContent: 'flex-start',
+    marginBottom: '2%'
+  },
+  tab: {
+    textTransform: 'none',
+    fontSize: 16,
+    color: palette.greyDark
+  },
+  inputConfirmMdp: {
+    justifySelf: 'flex-end'
+  },
+  inputAncienMdp: {
+    width: '47%'
   }
 }))
 
-const Profil = () => {
-  const classes = useStyle()
+function TabPanel(props) {
+  const { children, value, index, ...other } = props
 
   return (
-    <>
+    <Typography component="div" role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
+      <Box p={3}>{children}</Box>
+    </Typography>
+  )
+}
+
+function a11yProps(index) {
+  return {
+    id: `tab-${index}`,
+    'aria-controls': `tabpanel-${index}`
+  }
+}
+
+const Profil = () => {
+  const classes = useStyle()
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Container maxWidth="lg">
       <Paper className={classes.sectionProfil}>
-        <form className={classes.form}>
-          <Typography className={classes.title}>Informations personnelles</Typography>
-          <div className={classes.info}>
-            <TextField fullWidth className={classes.input} id="nom" label="NOM" defaultValue="Entrez votre nom ici" />
-            <TextField className={classNames(classes.input, classes.second)} fullWidth id="prenom" label="PRÉNOM" defaultValue="Entrez votre prénom ici" />
-          </div>
+        <Tabs value={value} onChange={handleChange} className={classes.appBar} indicatorColor="primary">
+          <Tab className={classes.tab} label="Informations personnelles" {...a11yProps(0)} />
+          <Tab className={classes.tab} label="Mot de passe" {...a11yProps(1)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <form className={classes.form}>
+            <div className={classes.info}>
+              <TextField fullWidth className={classes.input} id="nom" label="NOM" defaultValue="Entrez votre nom ici" />
+              <TextField className={classNames(classes.input, classes.second)} fullWidth id="prenom" label="PRÉNOM" defaultValue="Entrez votre prénom ici" />
+            </div>
 
-          <div className={classes.info}>
-            <TextField fullWidth className={classes.input} id="email" label="E-MAIL" defaultValue="Entrez votre e-mail ici" />
+            <div className={classes.info}>
+              <TextField className={classes.input} id="email" label="E-MAIL" defaultValue="Entrez votre e-mail ici" />
 
-            <TextField
-              className={classNames(classes.input, classes.second)}
-              fullWidth
-              id="mdp"
-              label="MOT DE PASSE"
-              type="password"
-              defaultValue="Entrez votre mot de passe ici"
+              <TextField fullWidth className={classNames(classes.input, classes.second)} id="username" label="PSEUDO" defaultValue="Entrez votre pseudo ici" />
+            </div>
+            <FormControlLabel
+              className={classes.newsletter}
+              value="newsletterComposteur"
+              label="S'abonner à la newsletter du composteur"
+              control={<Switch color="primary" />}
+              labelPlacement="end"
             />
-          </div>
-          <FormControlLabel
-            className={classes.newsletter}
-            value="newsletter"
-            label="S'abonner à la newsletter"
-            control={<Switch color="primary" />}
-            labelPlacement="end"
-          />
 
-          <Button className={classes.buttonSubmit} type="submit" variant="contained" color="primary">
-            Modifier mes informations
-          </Button>
-        </form>
+            <FormControlLabel
+              className={classes.newsletter}
+              value="newsletter"
+              label="S'abonner à la newsletter de Compostri"
+              control={<Switch color="primary" />}
+              labelPlacement="end"
+            />
+
+            <Button className={classes.buttonSubmit} type="submit" variant="contained" color="primary">
+              Modifier mes informations
+            </Button>
+          </form>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <form className={classes.form}>
+            <div className={classes.info}>
+              <TextField
+                InputLabelProps={{
+                  shrink: true
+                }}
+                fullWidth
+                className={classNames(classes.input, classes.inputAncienMdp)}
+                id="lastMdp"
+                type="password"
+                label="ANCIEN MOT DE PASSE"
+                placeholder="Entrez votre ancien mot de passe"
+              />
+            </div>
+            <div className={classes.info}>
+              <TextField
+                InputLabelProps={{
+                  shrink: true
+                }}
+                className={classes.input}
+                id="newMdp"
+                type="password"
+                label="NOUVEAU MOT DE PASSE"
+                placeholder="Entrez votre nouveau mot de passe"
+              />
+              <TextField
+                InputLabelProps={{
+                  shrink: true
+                }}
+                placeholder="Confirmez votre nouveau mot de passe"
+                fullWidth
+                className={classNames(classes.inputConfirmMdp, classes.second, classes.input)}
+                id="confirmNewMdp"
+                type="password"
+                label="CONFIRMER MON MOT DE PASSE"
+              />
+            </div>
+            <Button className={classes.buttonSubmit} type="submit" variant="contained" color="primary">
+              Enregistrer le nouveau mot de passe
+            </Button>
+          </form>
+        </TabPanel>
       </Paper>
-    </>
+    </Container>
   )
 }
 
