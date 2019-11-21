bcd74089ca8d1e9242f056a93a751e7bb60b9611
JR <contact@jeanremypraud.com>
2019-11-21 11:56:28 +0100

    Gestion de l'inscription à la reception des formulaires

--- components/forms/composter/ContactForm.jsx
+++ components/forms/composter/ContactForm.jsx
@@ -1,16 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import * as Yup from 'yup'
 import { Formik, Form, Field, FieldArray } from 'formik'
-import { Box, FormGroup, FormControlLabel, Switch, Button, TextField, Grid, Typography } from '@material-ui/core'
+import { Box, FormGroup, FormControlLabel, Switch, Button, Typography, Chip, Avatar, CircularProgress } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import api from '~/utils/api'
-import ImageInput from '~/components/forms/ImageInput'
+import { UserContext } from '~context/UserContext'
+
+const ContactSchema = Yup.object().shape({
+  receive: Yup.boolean()
+})
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    marginBottom: theme.spacing(1)
+  },
+  receiver: {
+    marginRight: theme.spacing(1)
+  }
+}))
 
 const ContactForm = props => {
   const { composter, setSnackBarMessage, ...otherProps } = props
+  const [receivers, setReceivers] = useState()
+  const {
+    userContext: { user }
+  } = useContext(UserContext)
+
+  const getUserComposter = () =>
+    api.getUserComposter({ composter: composter.rid }).then(res => {
+      if (res.status === 200) {
+        // On stocke toutes les relations user <-> composteur
+        const rec = res.data['hydra:member']
+        setReceivers(rec)
+      }
+    })
+
+  useEffect(() => {
+    getUserComposter()
+  }, [composter.id])
+
+  const isReceiver = () => {
+    // Parmi toutes les relations, on filtre celles du user en cours et qui sont classées receivers
+    return receivers && receivers.filter(uc => uc.user['@id'] === `/users/${user.id}` && uc.composterContactReceiver === true).length > 0
+  }
+
+  const submit = async (values, { setSubmitting }) => {
+    // On cherche la relation du user en cours et on l'update
+    const uc = receivers.find(ucom => ucom.user['@id'])
+    api.updateUserComposter(uc['@id'], { composterContactReceiver: values.receive }).then(res => {
+      if (res.status === 200) {
+        getUserComposter()
+      }
+      setSubmitting(false)
+    })
+  }
+  const classes = useStyles()
+
   return (
     <Box {...otherProps}>
-      <Typography paragraph>Formulaire de contact</Typography>
+      <Formik
+        initialValues={{
+          receive: isReceiver()
+        }}
+        validationSchema={ContactSchema}
+        enableReinitialize
+        onSubmit={submit}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <Box my={2}>
+              <Typography>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id nulla eaque doloremque atque nesciunt incidunt ipsam rem perferendis molestiae?
+                Nesciunt aperiam iste quae eaque obcaecati. Corporis nam suscipit dolores doloremque!
+              </Typography>
+            </Box>
+            <Box my={2}>
+              <Typography variant="h2" className={classes.title}>
+                Liste des personnes recevant les formulaires de contact
+              </Typography>
+              <Box>
+                {receivers &&
+                  receivers
+                    .filter(uc => uc.composterContactReceiver === true)
+                    .map(uc => uc.user)
+                    .map(receiver => (
+                      <Chip className={classes.receiver} avatar={<Avatar>{receiver.username.substr(0, 1).toUpperCase()}</Avatar>} label={receiver.username} />
+                    ))}
+              </Box>
+            </Box>
+            <Box my={2}>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch value={values.receive} checked={values.receive} onChange={handleChange} />}
+                  label="Je veux recevoir les formulaires de contact"
+                  id="receive"
+                  name="receive"
+                  type="receive"
+                  className={classes.switchLabel}
+                />
+              </FormGroup>
+            </Box>
+            <Box my={2} align="center">
+              <Button type="submit" variant="contained" color="secondary">
+                {isSubmitting ? <CircularProgress /> : 'Enregistrer les modifications'}
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Box>
   )
 }
