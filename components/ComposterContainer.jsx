b33ae406b4b9a92eb01ad299cab7c28d44ced6eb
LucileB <lucileb@matierenoire.io>
2019-11-27 11:53:26 +0100

    composterContainer

--- components/ComposterContainer.jsx
+++ components/ComposterContainer.jsx
@@ -5,9 +5,8 @@ import Link from 'next/link'
 import classnames from 'classnames'
 import { useRouter } from 'next/router'
 import Header from '~/components/Header'
-import AbilityProvider from '~/context/AbilityContext'
 import ComposterProvider from '~/context/ComposterContext'
-import { Can, Action, Subject } from '~/context/AbilityContext'
+import AbilityProvider, { Can, Action, Subject } from '~/context/AbilityContext'
 import palette from '~/variables'
 
 const useStyles = makeStyles(theme => ({
