5bf743c7507aaa14da02edddc45fea57e6ea6657
LucileB <lucileb@matierenoire.io>
2019-11-27 11:29:30 +0100

    ajout Header

--- components/ComposterContainer.jsx
+++ components/ComposterContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
-import { Container } from '@material-ui/core'
-import classesname from 'classnames'
-import Header from '~/components/ComposterHeader'
+import { Container, Button } from '@material-ui/core'
+import Link from 'next/link'
+import classnames from 'classnames'
+import { useRouter } from 'next/router'
+import Header from '~/components/Header'
 import AbilityProvider from '~/context/AbilityContext'
 import ComposterProvider from '~/context/ComposterContext'
+import { Can, Action, Subject } from '~/context/AbilityContext'
+import palette from '~/variables'
 
 const useStyles = makeStyles(theme => ({
   wrapper: {
@@ -22,18 +26,74 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center'
+  },
+  button: {
+    color: palette.greyDark,
+    fontSize: '16px',
+    fontWeight: '400',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: 3,
+    borderBottomColor: 'transparent',
+    marginRight: theme.spacing(4),
+    borderRadius: 0,
+    '&:hover, &:focus': {
+      borderBottomColor: palette.greenPrimary,
+      backgroundColor: 'white'
+    }
+  },
+  activeButton: {
+    borderBottomColor: palette.greenPrimary,
+    fontWeight: 700,
+    backgroundColor: 'white'
   }
 }))
 
 const ComposterContainer = ({ composter, maxWidth = false, children }) => {
   const classes = useStyles()
-
+  const { READ } = Action
+  const router = useRouter
+  const { COMPOSTER_PERMANENCES, COMPOSTER_LISTES_OUVREURS, COMPOSTER_NEWLETTERS } = Subject
+  const { slug, permanencesRule } = composter
   return (
     <ComposterProvider composter={composter}>
-      <div className={classesname({ [classes.wrapper]: maxWidth })}>
+      <div className={classnames({ [classes.wrapper]: maxWidth })}>
         <AbilityProvider composterSlug={composter.slug}>
-          <Header />
-          <Container maxWidth={maxWidth} className={classesname(classes.container, { [classes.containerCenter]: maxWidth })}>
+          <Header title={composter.name}>
+            <div className={classes.toolbarLink}>
+              <Link href="/composter/[slug]" as={`/composter/${slug}`} passHref>
+                <Button disableRipple className={classnames(classes.button, { [classes.activeButton]: router.pathname === '/composter/[slug]' })}>
+                  Informations
+                </Button>
+              </Link>
+              <Can I={READ} this={{ $type: COMPOSTER_PERMANENCES, permanencesRule }}>
+                <Link href="/composter/[slug]/permanences" as={`/composter/${slug}/permanences`} passHref>
+                  <Button disableRipple className={classnames(classes.button, { [classes.activeButton]: router.pathname === '/composter/[slug]/permanences' })}>
+                    Permanences
+                  </Button>
+                </Link>
+              </Can>
+              <Link href="/composter/[slug]/statistiques" as={`/composter/${slug}/statistiques`} passHref>
+                <Button disableRipple className={classnames(classes.button, { [classes.activeButton]: router.pathname === '/composter/[slug]/statistiques' })}>
+                  Stastiques
+                </Button>
+              </Link>
+              <Can I={READ} this={COMPOSTER_LISTES_OUVREURS}>
+                <Link href="/composter/[slug]/ouvreurs" as={`/composter/${slug}/ouvreurs`} passHref>
+                  <Button disableRipple className={classnames(classes.button, { [classes.activeButton]: router.pathname === '/composter/[slug]/ouvreurs' })}>
+                    Listes d'ouvreurs
+                  </Button>
+                </Link>
+              </Can>
+              <Can I={READ} this={COMPOSTER_NEWLETTERS}>
+                <Link href="/composter/[slug]/newsletter" as={`/composter/${slug}/newsletter`} passHref>
+                  <Button disableRipple className={classnames(classes.button, { [classes.activeButton]: router.pathname === '/composter/[slug]/newsletter' })}>
+                    Newsletter
+                  </Button>
+                </Link>
+              </Can>
+            </div>
+          </Header>
+          <Container maxWidth={maxWidth} className={classnames(classes.container, { [classes.containerCenter]: maxWidth })}>
             {children}
           </Container>
         </AbilityProvider>
