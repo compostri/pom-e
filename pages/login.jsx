df88dfd6b631e466d474005456be4997fc67ac40
JR <contact@jeanremypraud.com>
2019-12-09 15:46:17 +0100

    responsive login

--- pages/login.jsx
+++ pages/login.jsx
@@ -27,7 +27,10 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    padding: theme.spacing(4, 8)
+    padding: theme.spacing(4, 8),
+    [theme.breakpoints.down('md')]: {
+      padding: theme.spacing(2)
+    }
   },
   h1: {
     color: palette.greyDark,
