836f139443cf40d2ec0372f4554b78ef78012644
Arnaud Banvillet <arnaud@matierenoire.io>
2019-12-19 16:30:20 +0100

    Add offline export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const nextEnv = require('next-env')
 const dotenvLoad = require('dotenv-load')
+const withOffline = require('next-offline')
 
 dotenvLoad()
 
 const withNextEnv = nextEnv()
 
-module.exports = withNextEnv({
-  // Your Next.js config.
-})
+module.exports = withNextEnv(
+  withOffline({
+    // Your Next.js config.
+  })
+)
