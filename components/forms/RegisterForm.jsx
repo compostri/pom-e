2a0a358c943822ec40ceb463890cde896017ea5c
ArnaudBan <arnaud@matierenoire.io>
2021-02-25 10:40:41 +0100

    fix: ajouté l’inscription a la newsletter

--- components/forms/RegisterForm.jsx
+++ components/forms/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Typography, Button, TextField, Select, Box, CircularProgress, Grid, MenuItem } from '@material-ui/core'
+import { Typography, Button, TextField, Select, Switch, FormControlLabel, InputLabel, Box, CircularProgress, Grid, MenuItem } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import * as Yup from 'yup'
 
@@ -18,7 +18,14 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const [Email, Username, Firstname, Lastname, Capability] = ['user.email', 'user.username', 'user.firstname', 'user.lastname', 'capability']
+const [Email, Username, Firstname, Lastname, Capability, Newsletter] = [
+  'user.email',
+  'user.username',
+  'user.firstname',
+  'user.lastname',
+  'capability',
+  'newsletter'
+]
 
 const RegisterSchema = Yup.object().shape({
   user: Yup.object().shape({
@@ -42,55 +49,93 @@ const initialValues = {
       .toString(36)
       .replace(/[^a-z]+/g, '')
   },
-  [Capability]: ''
+  [Capability]: 'User',
+  [Newsletter]: true
 }
 
 const FormikTextField = withFormikField(TextField)
+const FormikSwitchField = withFormikField(Switch)
 const FormikSelectField = withFormikField(Select)
 
 const buildFields = fields => {
-  const buildField = (errors, touched, values) => ({ label, name }) =>
-    name === Capability ? (
-      values[name] !== 'Referent' && (
-        <Grid item xs={6} key={name}>
-          <FormikSelectField
-            required
-            fullWidth
-            InputLabelProps={{
-              shrink: true
-            }}
-            label={label}
-            name={name}
-            helperText={errors[name] && touched[name] ? errors[name] : null}
-          >
-            <MenuItem value="Opener">Ouvreur</MenuItem>
-            <MenuItem value="User">Utilisateur</MenuItem>
-          </FormikSelectField>
-        </Grid>
-      )
-    ) : (
-      <Grid item xs={6} key={name}>
-        <FormikTextField
-          required
-          fullWidth
-          InputLabelProps={{
-            shrink: true
-          }}
-          label={label}
-          name={name}
-          helperText={errors[name] && touched[name] ? errors[name] : null}
-        />
-      </Grid>
-    )
+  const buildField = (errors, touched, values) => ({ label, name, type, choices }) => {
+    let field = ''
+    switch (type) {
+      case 'select':
+        field = (
+          <Grid item xs={6} key={name}>
+            <InputLabel shrink={true} id={name}>
+              {label}
+            </InputLabel>
+            <FormikSelectField
+              required
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+              labelId={name}
+              name={name}
+              helperText={errors[name] && touched[name] ? errors[name] : null}
+            >
+              {choices.map(({ value, label }) => (
+                <MenuItem value={value}>{label}</MenuItem>
+              ))}
+            </FormikSelectField>
+          </Grid>
+        )
+        break
+      case 'text':
+        field = (
+          <Grid item xs={6} key={name}>
+            <FormikTextField
+              required
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+              label={label}
+              name={name}
+              helperText={errors[name] && touched[name] ? errors[name] : null}
+            />
+          </Grid>
+        )
+        break
+      case 'boolean':
+        field = (
+          <Grid item xs={6} key={name}>
+            <FormControlLabel
+              control={<FormikSwitchField checked={values[name]} name={name} required />}
+              label={label}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </Grid>
+        )
+        break
+    }
+
+    return field
+  }
+
   return (errors, touched, values) => fields.map(buildField(errors, touched, values))
 }
 
 const renderFields = buildFields([
-  { label: 'Email', name: Email },
-  { label: 'Pseudo', name: Username },
-  { label: 'Nom', name: Lastname },
-  { label: 'Prénom', name: Firstname },
-  { label: 'Role', name: Capability }
+  { label: 'Email', name: Email, type: 'text' },
+  { label: 'Pseudo', name: Username, type: 'text' },
+  { label: 'Nom', name: Lastname, type: 'text' },
+  { label: 'Prénom', name: Firstname, type: 'text' },
+  {
+    label: 'Role',
+    name: Capability,
+    type: 'select',
+    choices: [
+      { value: 'Opener', label: 'Ouvreur' },
+      { value: 'User', label: 'Utilisateur' }
+    ]
+  },
+  { label: 'Newsletter', name: Newsletter, type: 'boolean' }
 ])
 
 const propTypes = {
